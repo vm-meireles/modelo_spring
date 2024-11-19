095d8e1d10d8ebbcd23d08f727fbd0b49d33bb8d
vm-meireles <victo6236@gmail.com>
2024-11-19 15:43:56 -0300

    Corrigir

---
+++ modelo_evento/src/main/java/com/meireles/modelo_evento/Controller/RouteController.java
@@ -0,0 +1,14 @@
+package com.meireles.modelo_evento.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller 
+public class RouteController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
