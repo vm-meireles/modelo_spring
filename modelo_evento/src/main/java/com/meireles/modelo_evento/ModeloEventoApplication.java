52c170ab7648715bd3188089c7566ade7bdcecc5
vm-meireles <victo6236@gmail.com>
2024-11-19 15:32:51 -0300

    Levantar Git

---
+++ modelo_evento/src/main/java/com/meireles/modelo_evento/ModeloEventoApplication.java
@@ -0,0 +1,13 @@
+package com.meireles.modelo_evento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloEventoApplication.class, args);
+	}
+
+}
---
+++ modelo_evento/src/test/java/com/meireles/modelo_evento/ModeloEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meireles.modelo_evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
